3a56a3248e4a09508c1224a0106704515f4ec9e8
Jesus390 <jesusespinola90@hotmail.com>
2022-01-20 17:59:11 -0300

    se creo la pagina de error, coneccion a base de datos.

--- home.php
+++ home.php
@@ -1,10 +1,8 @@
 <?php require_once('header.php');?>
 <?php require_once('menu.php');?>
 
-
-
 <?php
-if($result = $coneccion->query("SELECT * FROM posts ORDER BY idposts DESC LIMIT 10")) {
+if($result = $coneccion->query("SELECT * FROM posts ORDER BY idposts DESC LIMIT 3")) {
     if($result->num_rows == 0) {
         echo '<section class="info-blog">
             <h3>No hay publicaciones</h3>
@@ -31,8 +29,12 @@ $coneccion->close();
 ?>
 
 
-
+<section class="network">
+    <h3>Mis Redes</h3>
+    <ul>
+        <li><a href="https://github.com/Jesus390" target="_blank" title="Go my github">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/marioespinola/" target="_blank" title="Go my Linkedin">linkedin</a></li>
+    </ul>
+</section>
 
 <?php require_once('footer.php');?>
-
-
