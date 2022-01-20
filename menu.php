ff9020f4595f6b3f85cac152c3a5898012e90cd9
Jesus390 <jesusespinola90@hotmail.com>
2022-01-20 13:24:34 -0300

    se creo el archivo menu.php

---
+++ menu.php
@@ -0,0 +1,13 @@
+<section class="container me">
+    <h1><span>Hi, my name's</span> Mario Espinola</h1>
+    <p>
+        I'm only a programmer pasionate for the informatic technology, specially Machine Learning, Neural Network, Data Mining and Artificial Intelligence.
+    </p>
+</section>
+<hr>
+<section class="menu">
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/blog">Blog</a></li>
+    </ul>
+</section>
